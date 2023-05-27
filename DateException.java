b7319ffbd91c125ab3bfd54b56ce3cadefd3da7c
Guccher <Guccher_007@mail.ru>
2023-05-27 14:28:06 +0300

    AddedDateFile

---
+++ DateException.java
@@ -0,0 +1,15 @@
+package Exceptions_in_programming;
+
+public class DateException extends Exception {
+    public DateException() {
+    }
+
+public void dataException(String i) {
+    System.out.println("Exception: DateNotCorrectFormat");
+    System.out.printf("It`s not correct date format: %s", i);
+    System.out.println();
+
+
+
+    }
+}
